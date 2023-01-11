0a9e3741fae5281a5db2d317a596defc8c5f9d4b
Bruno-1333 <2295132rcegepoutaouais.qc.ca>
2023-01-11 15:55:28 -0500

    Version avec javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function ajouter(){
+var item = document.getElementById("desc" ).value;
+document.getElementById("maliste").append(item);
+    alert("l'iem est bien ajouter"); // pop-up
+}
+
+
+
+
 console.log("Bonjour");
+
