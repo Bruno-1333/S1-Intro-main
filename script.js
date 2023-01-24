bf98f1a1ff6ddd204675d5221cd9048bb6586ee0
Bruno-1333 <2295132rcegepoutaouais.qc.ca>
2023-01-24 10:22:48 -0500

    Correction script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Bonjour");
+compteur = 4;
+
+function Ajouter(){
+    let item = document.getElementById("desc").value;
+    let prix = document.getElementById("valeur").value; // Ajouter prix
+    let nouveauP = document.createElement("p");         // Créer une balise <p>
+    let nouveauSpan = document.createElement("span");
+    nouveauP.innerText = item+ " : ";
+    nouveauSpan.innerText = prix + "$";
+    document.getElementById("maList").append(nouveauP);   // Ajouter le paragraphe à la liste
+    nouveauP.append(nouveauSpan);                                   // mettre le span à la fin du p
+
+    // Mettre à jour le total
+    let ancienPrix = document.getElementById("total").innerText;
+    let nouveauPrix = +ancienPrix + +prix;                          // +variable : convertir en numérique
+    document.getElementById("total").innerText = nouveauPrix;
+
+    nouveauInput = document.createElement("input");
+    nouveauInput.setAttribute("hidden", value true);
+
+
+
+
+
+    function ajouterEmail(){
+        var email = document.getElementById("email").value;
+        document.forms[0].action = "mailto"+email;
+    }
+}
